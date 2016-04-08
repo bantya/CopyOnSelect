fb2c6ff4ce27ac1a0ad41524c5556b71b34d4530
chrifpa <chrifpa@gmail.com>
2016-04-08 09:38:51 +0200

    Fixed(?) Loading of settings
    I'm considering removing the settings, because that part of the plugin keeps crashing and I can't tell why. Apparently the value returned by the settings object is not what it should be, an int. Apparently, sometimes it can't be converted to an int, so then I have to fall back to a default. I *think* it should work now, but I'm not sure because it always takes some time to fail and I haven't pinned down yet what causes the problem. With a freshly installed plugin it always works, and then *something* happens and all of a sudden it doesn't.
    
    Anyway, should at least not stop working now. I think. I hope.

--- copy-on-select.py
+++ copy-on-select.py
@@ -5,7 +5,16 @@ import functools
 
 class CopyOnSelectCommand(sublime_plugin.EventListener):
 
-    DELAY = int(sublime.load_settings("CopyOnSelect.sublime-settings").get("delay", 1000))
+    @property
+    def delay(self):
+        settings = sublime.load_settings("CopyOnSelect.sublime-settings")
+        if settings:
+            delay = settings.get("delay", 1000)
+            try:
+                return int(delay)
+            except:
+                pass
+        return 1000
 
     # number of pending calls to handle_timeout
     pending = 0
@@ -14,13 +23,13 @@ class CopyOnSelectCommand(sublime_plugin.EventListener):
         self.pending = self.pending - 1
         if self.pending == 0:
             # There are no more queued up calls to handle_timeout, so it must have
-            # been 1000ms since the last modification
+            # been {delay}ms since the last modification
             self.on_idle(view)
 
     def on_selection_modified(self, view):
         self.pending = self.pending + 1
         # Ask for handle_timeout to be called in DELAY ms
-        sublime.set_timeout(functools.partial(self.handle_timeout, view), self.DELAY)
+        sublime.set_timeout(functools.partial(self.handle_timeout, view), self.delay)
 
     def on_idle(self, view):
         string = ""
